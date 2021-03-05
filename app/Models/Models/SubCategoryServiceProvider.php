c6175b066bb17e22bd72553cba9673275ed4533a
Amrita <amritapoudel016@gmail.com>
2021-03-05 22:43:21 +0545

    Admin created

--- app/Models/Models/SubCategoryServiceProvider.php
+++ app/Models/Models/SubCategoryServiceProvider.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class SubCategoryServiceProvider extends Model
 {
     use HasFactory;
+     protected $fillable = [
+        'service_provider_id',
+        'subcategories_id',
+        
+    ];
 }
