cd97105c358d5d9a13e12b386a379ec19d4fb8de
bishal <devkotahancy@gmail.com>
2021-03-13 14:47:48 +0545

    changed state

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,10 +13,13 @@ class SearchController extends Controller
 {
     public function Search($q)
     {
-    // $user = User::where('username','LIKE','%'.$q.'%')->orWhere('email','LIKE','%'.$q.'%')->get();
-    // $category = Category::where('name','LIKE','%'.$q.'%')->orWhere('description','LIKE','%'.$q.'%')->get();
-    // $subcategory = SubCategory::where('name','LIKE','%'.$q.'%')->orWhere('description','LIKE','%'.$q.'%')->get();
-    // $service = Service::where('name','LIKE','%'.$q.'%')->orWhere('description','LIKE','%'.$q.'%')->get();
-    // $operation = Operation::where('address_address','LIKE','%'.$q.'%')->orWhere('start_time','LIKE','%'.$q.'%')->orWhere('end_time','LIKE','%'.$q.'%')->orWhere('completed_time','LIKE','%'.$q.'%')->where('end_time','LIKE','%'.$q.'%')->get();
-    }
+   $category = Category::where('name','LIKE','%'.$q.'%')->orWhere('description','LIKE','%'.$q.'%')->get();
+    $subcategory = SubCategory::where('name','LIKE','%'.$q.'%')->orWhere('description','LIKE','%'.$q.'%')->get();
+    $service = Service::where('name','LIKE','%'.$q.'%')->orWhere('description','LIKE','%'.$q.'%')->get();
+    //$operation = Operation::where('address_address','LIKE','%'.$q.'%')->orWhere('start_time','LIKE','%'.$q.'%')->orWhere('end_time','LIKE','%'.$q.'%')->orWhere('completed_time','LIKE','%'.$q.'%')->where('end_time','LIKE','%'.$q.'%')->get();
+    return response(
+            ['category' => $category,
+            'subcategory'=>$subcategory,
+    		'service'=>$service],200);
+}
 }
