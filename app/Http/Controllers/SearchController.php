5404d410bf2997b29656c047caef7aa630229f44
Bishal Devkota <devkotahancy@gmail.com>
2021-04-08 10:56:48 +0545

    changer

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,8 +14,8 @@ class SearchController extends Controller
     public function Search()
     {
    $category = Category::select('name')->get();
-    $subcategory = SubCategory::select('name')->where('name','LIKE','%'.$q.'%')->orWhere('description','LIKE','%'.$q.'%')->get();
-    $service = Service::select('name')->where('name','LIKE','%'.$q.'%')->orWhere('description','LIKE','%'.$q.'%')->get();
+    $subcategory = SubCategory::select('name')->get();
+    $service = Service::select('name')->get();
     //$operation = Operation::where('address_address','LIKE','%'.$q.'%')->orWhere('start_time','LIKE','%'.$q.'%')->orWhere('end_time','LIKE','%'.$q.'%')->orWhere('completed_time','LIKE','%'.$q.'%')->where('end_time','LIKE','%'.$q.'%')->get();
     return response(
             ['category' => $category,
