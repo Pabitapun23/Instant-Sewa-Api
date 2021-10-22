53a7b512a1abd0207cce4e5aabe98be89bf1ea97
Bishal <bishalf98@gmail.com>
2021-10-22 17:29:43 +0545

    some changed for map

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -10,34 +10,34 @@ class TrackerController extends Controller
 {
 	 public function CompletedTracker(Request $request)
     {
-      $services = DB::table('operations')->select('cart_collection_id','id','start_time')->where('service_user_id',$request->user()->id)->where('completed_flag','1')->orderBy('start_time', 'asc')->get();
+      $services = DB::table('operations')->select('cart_collection_id','id','start_time','address_latitude','address_longitude')->where('service_user_id',$request->user()->id)->where('completed_flag','1')->orderBy('start_time', 'asc')->get();
        return new OngoingTrackerCollection($services);
     }
      public function OngoingTracker(Request $request)
     {
-      $services = DB::table('operations')->select('cart_collection_id','id','start_time')->where('service_user_id',$request->user()->id)->where('completed_flag','0')->where('cancel_flag','0')->orderBy('start_time', 'asc')->get();
+      $services = DB::table('operations')->select('cart_collection_id','id','start_time','address_latitude','address_longitude')->where('service_user_id',$request->user()->id)->where('completed_flag','0')->where('cancel_flag','0')->orderBy('start_time', 'asc')->get();
        return new OngoingTrackerCollection($services);
     }
 
      public function CancelledTracker(Request $request)
     {
-     $services = DB::table('operations')->select('cart_collection_id','id','start_time')->where('service_user_id',$request->user()->id)->where('cancel_flag','1')->orderBy('start_time', 'asc')->get();
+     $services = DB::table('operations')->select('cart_collection_id','id','start_time','address_latitude','address_longitude')->where('service_user_id',$request->user()->id)->where('cancel_flag','1')->orderBy('start_time', 'asc')->get();
        return new OngoingTrackerCollection($services);
     }
 
      public function ProviderCancelledTracker(Request $request)
     {
-      $services = DB::table('operations')->select('cart_collection_id','id','start_time')->where('service_provider_id',$request->user()->id)->where('cancel_flag','1')->orderBy('start_time', 'asc')->get();
+      $services = DB::table('operations')->select('cart_collection_id','id','start_time','address_latitude','address_longitude')->where('service_provider_id',$request->user()->id)->where('cancel_flag','1')->orderBy('start_time', 'asc')->get();
        return new OngoingTrackerCollection($services);
     }
     public function ProviderCompletedTracker(Request $request)
     {
-      $services = DB::table('operations')->select('cart_collection_id','id','start_time')->where('service_provider_id',$request->user()->id)->where('completed_flag','1')->orderBy('start_time', 'asc')->get();
+      $services = DB::table('operations')->select('cart_collection_id','id','start_time','address_latitude','address_longitude')->where('service_provider_id',$request->user()->id)->where('completed_flag','1')->orderBy('start_time', 'asc')->get();
        return new OngoingTrackerCollection($services);
     }
      public function ProviderOngoingTracker(Request $request)
     {
-      $services = DB::table('operations')->select('cart_collection_id','id','start_time')->where('service_provider_id',$request->user()->id)->where('completed_flag','0')->where('cancel_flag','0')->orderBy('start_time', 'asc')->get();
+      $services = DB::table('operations')->select('cart_collection_id','id','start_time','address_latitude','address_longitude')->where('service_provider_id',$request->user()->id)->where('completed_flag','0')->where('cancel_flag','0')->orderBy('start_time', 'asc')->get();
        return new OngoingTrackerCollection($services);
     }
       public static function CartCollectionName($id)
