6e4584f3105fda641d8ddb5bce8ed929330dcdb4
Pabita Pun <pabita.pun23@gmail.com>
2021-11-07 16:03:37 +0545

    categories sql

--- sql file/categories.sql
+++ sql file/categories.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2020 at 07:42 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Nov 07, 2021 at 11:17 AM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -21,20 +21,51 @@ SET time_zone = "+00:00";
 -- Database: `instantsewa`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `name`, `image`) VALUES
-(1, 'Electrical', 'images/photos/electric.png'),
-(2, 'Painting', 'images/photos/painting1.png'),
-(3, 'Plumbing', 'images/photos/plumb.png'),
-(4, 'Beauty', 'images/photos/beautician.png'),
-(5, 'Cleaning', 'images/photos/clean2.png'),
-(6, 'Appliances', 'images/photos/appliance1.png'),
-(7, 'Laundry', 'images/photos/laundry.png'),
-(8, 'Tv mounting', 'images/photos/tvMounting.png'),
-(9, 'Handyman', 'images/photos/plumb1.png');
+INSERT INTO `categories` (`id`, `name`, `description`, `image`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Electrical', '', 'images/photos/electric.png', NULL, NULL, NULL),
+(2, 'Painting', '', 'images/photos/painting1.png', NULL, NULL, NULL),
+(3, 'Plumbing', '', 'images/photos/plumb.png', NULL, NULL, NULL),
+(4, 'Cleaning', '', 'images/photos/clean2.png', NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
