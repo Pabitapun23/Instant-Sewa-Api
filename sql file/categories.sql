ef411275eda2a8fa3ce84ca7aba0c14ceba1093b
Pabita Pun <pabita.pun23@gmail.com>
2021-11-08 14:15:06 +0545

    sql update

--- sql file/categories.sql
+++ sql file/categories.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2021 at 11:17 AM
+-- Generation Time: Nov 08, 2021 at 09:28 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -21,22 +21,6 @@ SET time_zone = "+00:00";
 -- Database: `instantsewa`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
-CREATE TABLE `categories` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Dumping data for table `categories`
 --
@@ -46,26 +30,6 @@ INSERT INTO `categories` (`id`, `name`, `description`, `image`, `created_at`, `u
 (2, 'Painting', '', 'images/photos/painting1.png', NULL, NULL, NULL),
 (3, 'Plumbing', '', 'images/photos/plumb.png', NULL, NULL, NULL),
 (4, 'Cleaning', '', 'images/photos/clean2.png', NULL, NULL, NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
